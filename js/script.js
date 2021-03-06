69ffd5c919f26857b57936d29b6f5dc3c16d2462
Burence1 <burensomondi@gmail.com>
2021-03-07 00:00:33 +0300

    redesign order form

--- js/script.js
+++ js/script.js
@@ -1,7 +1,39 @@
 $(document).ready(function () {
-  // Activate Carousel
-  $("#carousel").carousel();
+  $("#type").click(function () {
+    $("#size").slideDown(800);
+  })
+  $("#size").click(function () {
+    $("#crust").slideDown(800);
+  })
+  $("#crust").click(function () {
+    $("#toppings").slideDown(800);
+  })
+  $("#toppings").click(function () {
+    $("#mode").slideDown(800);
+  })
+  $("#mode").click(function () {
+    $("#button").slideDown(800);
+  })
 });
-$("#item1").click(function () {
-  $("#carousel").carousel(2);
-});
+
+
+//front-end
+class Total {
+  constructor(sizeCost, crust, toppings, delivery) {
+    this.sizeCost = sizeCost;
+    this.crust = crust;
+    this.toppings = toppings;
+    this.delivery = delivery;
+    this.toppings = toppings;
+  }
+  totalCost() {
+    return ((this.sizeCost + this.crust + this.toppings + this.delivery) * number);
+  }
+}
+function toppings(){
+ var number = 0;
+ number = toppings;
+
+ return (toppings * 50);
+}
+
